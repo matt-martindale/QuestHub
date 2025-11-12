5016a7dc8c5b3dbe1436379e5df7a75248d71925
Matt Martindale <matt.martindale88@gmail.com>
2025-11-11 20:44:16 -0700

    Restore IDGenerator

--- QuestHub/QuestHub/Utilities/IDGenerator.swift
+++ QuestHub/QuestHub/Utilities/IDGenerator.swift
@@ -4,8 +4,8 @@ enum IDGenerator {
     /// Generates a short, URL-safe ID using an unambiguous character set.
     /// - Parameter length: Desired length of the generated ID. Default is 6.
     /// - Returns: A random string of the requested length.
-    static func makeShortID(length: Int = 1) -> String {
-        let characters = Array("ABC")
+    static func makeShortID(length: Int = 4) -> String {
+        let characters = Array("ABCDEFGHJKLMNPQRSTUVWXYZ23456789")
         var result = String()
         result.reserveCapacity(length)
         for _ in 0..<length {
