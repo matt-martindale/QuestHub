fc3b89ea22f7bba4638661e7ccb61dd8837a0c5d
Matt Martindale <matt.martindale88@gmail.com>
2025-11-13 14:03:22 -0700

    Create playQuest view

---
+++ QuestHub/QuestHub/Views/PlayQuest/PlayQuestView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayQuestView.swift
+//  QuestHub
+//
+//  Created by Matt Martindale on 11/13/25.
+//
+
+import SwiftUI
+
+struct PlayQuestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayQuestView()
+}
