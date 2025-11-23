a31011e3e613167bb05aa0584043d014d9afe248
Matt Martindale <matt.martindale88@gmail.com>
2025-11-23 11:24:57 -0700

    Update userQuests object

--- QuestHub/QuestHub/Models/UserQuest.swift
+++ QuestHub/QuestHub/Models/UserQuest.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 struct UserQuest: Codable, Identifiable, Hashable {
-    let id: String? // userId_questId
+    let id: String // userId_questId
     let questId: String?
     let userId: String?
     let questCode: String?
     let points: Int?
     let joinedAt: Date?
+    let challengeProgress: [String : ChallengeProgress]?
+}
+
+struct ChallengeProgress: Codable, Hashable {
+    let completed: Bool?
+    let challengePoints: Int?
+    let completedAt: Date?
+    let challengeResponse: String?
 }
